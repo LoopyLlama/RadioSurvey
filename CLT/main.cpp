1600960183a7d42d895f6b7aa16cdae6ee1ad77d
Jason Thomas <jthomas@Jasons-iMac.local>
2014-11-24 07:10:03 -0800

    Initial Commit

---
+++ CLT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CLT
+//
+//  Created by Jason Thomas on 11/24/14.
+//  Copyright (c) 2014 3LS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
